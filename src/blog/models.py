eb9035ee4ddef8d6464e3926e222e7e3ef1f16ac
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-09-20 18:39:30 +0300

    Semantic Search with Django and pgvector

--- src/blog/models.py
+++ src/blog/models.py
@@ -3,7 +3,6 @@ from pgvector.django import VectorField
 from sentence_transformers import SentenceTransformer
 
 
-EMBEDDING_MODEL = 'all-mpnet-base-v2'
 EMBEDDING_LENGTH = 768
 
 class BlogPost(models.Model):
@@ -11,3 +10,7 @@ class BlogPost(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     embedding = VectorField(dimensions=EMBEDDING_LENGTH, blank=True, null=True)
+    can_delete = models.BooleanField(default=False, help_text='Use in jupyter notebooks')
+    
+    def get_embedding_text_raw(self):
+        return self.content
