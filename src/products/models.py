8a8f5edecaf633701166d3c0893518795074ea59
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-09-21 14:31:36 +0300

    Creating the products app.

---
+++ src/products/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from pgvector.django import VectorField
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
+
+EMBEDDING_LENGTH = 768
+
+class Embedding(models.Model):
+    embedding = VectorField(dimensions=EMBEDDING_LENGTH, blank=True, null=True)
+    object_id = models.PositiveIntegerField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.SET_NULL, null=True)
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    embedding = GenericRelation(Embedding)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    can_delete = models.BooleanField(default=False, help_text='Use in jupyter notebooks')
+
+    
+    def get_embedding_text_raw(self):
+        return self.content
